7a8505cee7d7d8a2ec7d6d2b6d66fc0c776d52e8
Sergey Morozov <sergey@morozov.top>
2023-01-22 22:27:14 +0300

    Added missing file

---
+++ src/ui/colors.ts
@@ -0,0 +1,25 @@
+/// <reference types="../missing-blockbench-types/index" />
+import * as THREE from 'three';
+
+// TODO update
+const Colors = {
+    ui: new THREE.Color(CustomTheme.data.colors.ui),
+    back: new THREE.Color(CustomTheme.data.colors.back),
+    dark: new THREE.Color(CustomTheme.data.colors.dark),
+    border: new THREE.Color(CustomTheme.data.colors.border),
+    selected: new THREE.Color(CustomTheme.data.colors.selected),
+    button: new THREE.Color(CustomTheme.data.colors.button),
+    bright_ui: new THREE.Color(CustomTheme.data.colors.bright_ui),
+    accent: new THREE.Color(CustomTheme.data.colors.accent),
+    frame: new THREE.Color(CustomTheme.data.colors.frame),
+    text: new THREE.Color(CustomTheme.data.colors.text),
+    light: new THREE.Color(CustomTheme.data.colors.light),
+    accent_text: new THREE.Color(CustomTheme.data.colors.accent_text),
+    bright_ui_text: new THREE.Color(CustomTheme.data.colors.bright_ui_text),
+    subtle_text: new THREE.Color(CustomTheme.data.colors.subtle_text),
+    grid: new THREE.Color(CustomTheme.data.colors.grid),
+    wireframe: new THREE.Color(CustomTheme.data.colors.wireframe),
+    checkerboard: new THREE.Color(CustomTheme.data.colors.checkerboard),
+}
+
+export {Colors}
